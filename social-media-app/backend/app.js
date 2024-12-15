ff03c389cf499e53e4bcfed355fb9e54359aa550
zz22137 <zhang.zichen3@northeastern.edu>
2024-12-15 01:43:39 -0800

    Initial commit

--- social-media-app/backend/app.js
+++ social-media-app/backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/socialMediaApp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://reyesjesse22137:ZZC22137qaz@project3.hrtas.mongodb.net/?retryWrites=true&w=majority&appName=project3', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
